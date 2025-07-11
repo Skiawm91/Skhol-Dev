44ec7eda952c6e7fcf4200ccabec642609998a87
Skiawm91 <96339124+Skiawm91@users.noreply.github.com>
2025-07-11 13:55:47 +0800

    pkg2

--- SdoneOS/repo/packages/verifylist.bat
+++ SdoneOS/repo/packages/verifylist.bat
@@ -1,7 +1,7 @@
 set vldate=250112v1
 :: Updated at 2025/1/12 ::
-if exist %root%\system\packages\com.skholdev.test echo if "%%command%%" == "test" (call packages\com.skholdev.test\test.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo test >> packages\list.bat
-if exist %root%\system\packages\com.skholdev.sakosp echo if "%%command%%" == "sakosp" (call packages\com.skholdev.sakosp\app.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo sakosp >> packages\list.bat
-if exist %root%\system\packages\com.lvzbx.pkg echo if "%%command%%" == "pkg" (call packages\com.lvzbx.pkg\pkg.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo pkg (lvzbx) >> packages\list.bat
-if exist %root%\system\packages\com.ahqiang.hacknet echo if "%%command%%" == "hacknet" (call packages\com.ahqiang.hacknet\home.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo hacknet (ahqiang) >> packages\list.bat
-if exist %root%\system\packages\com.skholdev.syscustom echo if "%%command%%" == "syscustom" (call packages\com.skholdev.syscustom\app.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo syscustom >> packages\list.bat
+if exist %root%\system\packages\pkg.skholdev.test echo if "%%command%%" == "test" (call packages\pkg.skholdev.test\test.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo test >> packages\list.bat
+if exist %root%\system\packages\pkg.skholdev.sakosp echo if "%%command%%" == "sakosp" (call packages\pkg.skholdev.sakosp\app.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo sakosp >> packages\list.bat
+if exist %root%\system\packages\pkg.lvzbx.pkg echo if "%%command%%" == "pkg" (call packages\pkg.lvzbx.pkg\pkg.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo pkg (lvzbx) >> packages\list.bat
+if exist %root%\system\packages\pkg.ahqiang.hacknet echo if "%%command%%" == "hacknet" (call packages\pkg.ahqiang.hacknet\home.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo hacknet (ahqiang) >> packages\list.bat
+if exist %root%\system\packages\pkg.skholdev.syscustom echo if "%%command%%" == "syscustom" (call packages\pkg.skholdev.syscustom\app.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo syscustom >> packages\list.bat
